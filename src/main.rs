5f5810f6df4e23de3d1d641f2143b1d1319694a9
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-07 13:08:19 +0200

    refactor to handle multiple computations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -15,23 +16,35 @@ fn main() -> io::Result<()> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    let elapsed_regex = Regex::new(r"Validated state root.*elapsed=(\d+\.\d+)(ms|s)").unwrap();
-    let mut stats = Stats::new();
+    let regexes = vec![(
+        "state_root",
+        Regex::new(r"Validated state root.*elapsed=(\d+\.\d+)(ms|s)").unwrap(),
+    )];
+
+    let mut computations: HashMap<&str, Stats> = HashMap::new();
 
     for line in reader.lines() {
         let line = line?;
-        if let Some(caps) = elapsed_regex.captures(&line) {
-            if let (Some(matched_value), Some(unit)) = (caps.get(1), caps.get(2)) {
-                let mut elapsed: f64 = matched_value.as_str().parse().unwrap();
-                if unit.as_str() == "ms" {
-                    elapsed /= 1000.0;
+        for (label, regex) in &regexes {
+            if let Some(caps) = regex.captures(&line) {
+                if let (Some(matched_value), Some(unit)) = (caps.get(1), caps.get(2)) {
+                    let mut elapsed: f64 = matched_value.as_str().parse().unwrap();
+                    if unit.as_str() == "ms" {
+                        elapsed /= 1000.0;
+                    }
+                    computations
+                        .entry(label)
+                        .or_insert_with(Stats::new)
+                        .update(elapsed);
                 }
-                stats.update(elapsed);
             }
         }
     }
 
-    stats.print("State root computation");
+    for (label, stats) in computations {
+        println!("{}:", label);
+        stats.print();
+    }
 
     Ok(())
 }
@@ -67,10 +80,9 @@ impl Stats {
         }
     }
 
-    fn print(&self, label: &str) {
+    fn print(&self) {
         println!(
-            "{}: Average elapsed time = {:.3} s, Standard deviation = {:.3} s",
-            label,
+            "Average elapsed time = {:.3} s, Standard deviation = {:.3} s",
             self.mean,
             self.stddev()
         );
