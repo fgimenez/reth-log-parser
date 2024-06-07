be3cf1695daffb2a850881b658547a45f7badab7
Federico Gimenez <federico.gimenez@gmail.com>
2024-06-07 09:30:40 +0200

    split in submodules and use pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use eyre::Result;
-use log_parser::LogProcessor;
-use regex::Regex;
+use log_parser::log_processor::LogProcessor;
+use rayon::{prelude::*, ThreadPoolBuilder};
 use std::{
     env,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
+    sync::Arc,
 };
 
 fn main() -> Result<()> {
@@ -15,30 +16,35 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
+    // Configure the number of threads in the global rayon thread pool
+    ThreadPoolBuilder::new().num_threads(4).build_global()?; // Adjust the number of threads as needed
+
     let path = Path::new(&args[1]);
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    let regexes = vec![(
-        "state_root".to_string(),
-        Regex::new(r"Validated state root.*elapsed=(\d+\.\d+)(ms|s)")?,
-    )];
-    let mut processor = LogProcessor::new(regexes);
+    let processor = Arc::new(LogProcessor::new()?);
 
-    for line in reader.lines() {
-        let line = line?;
-        processor.process_line(&line)?;
+    reader.lines().par_bridge().for_each(|line| {
+        if let Ok(line) = line {
+            let processor = Arc::clone(&processor);
+            processor.process_line(&line).unwrap_or_else(|err| {
+                eprintln!("Error processing line: {}", err);
+            });
+        }
+    });
+
+    // Capture the last pipeline if it was still in progress
+    {
+        let mut pipelines = processor.pipelines.lock().unwrap();
+        let mut current_pipeline = processor.current_pipeline.lock().unwrap();
+        if let Some(pipeline) = current_pipeline.take() {
+            pipelines.push(pipeline);
+        }
     }
 
-    // Print results for each computation type
-    for (label, stats) in processor.get_computations() {
-        println!(
-            "{}: Average elapsed time = {:.3} s, Standard deviation = {:.3} s",
-            label,
-            stats.mean,
-            stats.stddev()
-        );
-    }
+    // Print the summary of each pipeline and the total aggregate duration
+    processor.print_summary();
 
     Ok(())
 }
