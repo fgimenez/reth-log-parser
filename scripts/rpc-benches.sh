69c0e11be073bb7175fb5ec2cb5c1213369a982f
Federico Gimenez <federico.gimenez@gmail.com>
2024-06-17 09:28:04 +0200

    rpc benches script

---
+++ scripts/rpc-benches.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+BENCHES_ID=${1:-reth-1.0-rc1}
+OUTPUT_BASE_PATH=${2:-/mnt/data/cryo}
+
+run_bench() {
+    local bench_name=${1}
+    local bench_command=${2}
+    cryo ${bench_command} \
+         --max-concurrent-requests 100 \
+         --rpc http://localhost:8545 \
+         --overwrite \
+         --output-dir ${OUTPUT_BASE_PATH}/reports > ${OUTPUT_BASE_PATH}/${BENCHES_ID}.${bench_name}.log
+}
+
+run_bench_blocks_to_tip(){
+    run_bench blocks_to_tip "blocks -b :20033500"
+}
+
+run_bench_blocks_range(){
+    run_bench blocks_range "blocks -b 19033400:20033500"
+}
+
+run_bench_tx_range1(){
+    run_bench tx_range1 "transactions -b 19793400:20033500"
+}
+
+run_bench_tx_range2(){
+    run_bench tx_range2 "transactions -b 20023500:20033500"
+}
+
+run_bench_logs_range1(){
+    run_bench logs_range1 "logs -b 18793400:19793400"
+}
+
+run_bench_logs_range2(){
+    run_bench logs_range2 "logs -b 19793400:20033500"
+}
+
+run_bench_state_reads(){
+    run_bench traces_state_reads "state_reads -b 20033436:20033500"
+}
+
+run_bench_geth_calls(){
+    run_bench traces_geth_calls "geth_calls -b 20033436:20033500"
+}
+
+main(){
+    run_bench_blocks_to_tip
+    run_bench_blocks_range
+    run_bench_tx_range1
+    run_bench_tx_range2
+    run_bench_logs_range1
+    run_bench_logs_range2
+    run_bench_state_reads
+    run_bench_geth_calls
+}
+
+main "${@}"
